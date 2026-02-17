a6d54af45a42637244e5ba4d30aeb33deeecf18e
Peter Graham <peterghm@gmail.com>
2026-02-16 20:04:58 -0500

    Update offense.rb

--- testdata/cops/lint/unreachable_loop/offense.rb
+++ testdata/cops/lint/unreachable_loop/offense.rb
@@ -16,3 +16,38 @@ loop do
   do_something
   break
 end
+
+# next in inner loop does NOT prevent outer loop from being flagged
+until x > 0
+^^^^^^^^^^^ Lint/UnreachableLoop: This loop will have at most one iteration.
+  items.each do |item|
+    next if item.odd?
+    break
+  end
+  if x > 0
+    break
+  else
+    raise MyError
+  end
+end
+
+# case-when-else with all branches breaking
+while x > 0
+^^^^^^^^^^^ Lint/UnreachableLoop: This loop will have at most one iteration.
+  case x
+  when 1
+    break
+  else
+    raise MyError
+  end
+end
+
+# if-else with all branches breaking
+while x > 0
+^^^^^^^^^^^ Lint/UnreachableLoop: This loop will have at most one iteration.
+  if condition
+    break
+  else
+    raise MyError
+  end
+end
